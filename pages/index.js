bccab9bac5663372fc7371d502cd44bda98c6034
Klyde-Sangalang <kvbsangalang.student@ua.edu.ph>
2023-08-26 23:39:30 +0800

    Secont Commit

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,14 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>What is your first impression as a 3rd year CPE Student?</p>
+        <p>My first impression as  a 3rd year CPE student is that I would be working
+          more on the major subjects and trying to implement the learning to a real life
+          scenarios.
+        </p>
+        <p>
+          I know now how to update my website!
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
