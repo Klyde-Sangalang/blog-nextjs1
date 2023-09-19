ad6c56f6d1ef106c0a0f1c3f54b3e92bab109320
Klyde-Sangalang <141467337+Klyde-Sangalang@users.noreply.github.com>
2023-09-19 17:09:00 +0800

    THE INDEX CHANGED

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,25 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Layout from '../../components/layout';
 
-export default function Home() {
+export default function FirstPost() {
   return (
-    <Layout home>
+    <Layout>
       <Head>
-        <title>{siteTitle}</title>
+        <title>First Post</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <h2><b>Manifesto for agile Software Development</b></h2><br></br>
-              
-          <h3>We are uncovering better ways of developing</h3><br></br>
-          <h3>software by doing it and helping others do it.</h3><br></br>
-          <h3>Through this work we have come to value:</h3><br></br>
-    <h2><b>Individuals and interactions</b> over processes and tools<h2><br></br>
-    <h2><b>Working software</b> over comprehensive documentation<h2><br></br>
-    <h2><b>Customer collaboration</b> over contract<h2><br></br>
-    <h2><b>Responding to change</b> over following a plan<h2><br></br>
-    <br></br>
-    <h2>That is, while there is value in the items on <br></br>
-    the right, we value the items on the left more. </h2>
-        
-      </section>
+<h2><b>Manifesto for agile Software Development</h2></b><br></br>
+<h3>We are uncovering better ways of developing</h3><br></br>
+<h3>software by doing it and helping others do it.</h3><br></br>
+<h3>Through this work we have come to value:</h3><br></br>
+<h2><b>Individuals and interactions</b> over processes and tools<h2><br></br>
+<h2><b>Working software</b> over comprehensive documentation<h2><br></br>
+<h2><b>Customer collaboration</b> over contract<h2><br></br>
+<h2><b>Responding to change</b> over following a plan<h2><br></br>
+<br></br>
+<h2>That is, while there is value in the items on <br></br>
+the right, we value the items on the left more. </h2>
+      </h2>
     </Layout>
   );
 }
