f4ad17993cbbc36bd6d8762ea824820a0d95baf2
Klyde-Sangalang <141467337+Klyde-Sangalang@users.noreply.github.com>
2023-09-19 17:06:05 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,15 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <h2><b>Manifesto for agile Software Development</b></h2><br></br>
               
-          <h3>We are uncovering better ways of developing</h3></br>
-          <h3>software by doing it and helping others do it.</h3></br>
-          <h3>Through this work we have come to value:</h3></br>
-    <h2><b>Individuals and interactions</b> over processes and tools<h2></br>
-    <h2><b>Working software</b> over comprehensive documentation<h2></br>
-    <h2><b>Customer collaboration</b> over contract<h2></br>
-    <h2><b>Responding to change</b> over following a plan<h2></br>
-    </br>
-    <h2>That is, while there is value in the items on </br>
+          <h3>We are uncovering better ways of developing</h3><br></br>
+          <h3>software by doing it and helping others do it.</h3><br></br>
+          <h3>Through this work we have come to value:</h3><br></br>
+    <h2><b>Individuals and interactions</b> over processes and tools<h2><br></br>
+    <h2><b>Working software</b> over comprehensive documentation<h2><br></br>
+    <h2><b>Customer collaboration</b> over contract<h2><br></br>
+    <h2><b>Responding to change</b> over following a plan<h2><br></br>
+    <br></br>
+    <h2>That is, while there is value in the items on <br></br>
     the right, we value the items on the left more. </h2>
         
       </section>
